1c67fef95f6c2a7d968f2c79f21d750a1993fa56
MarxNobita <489790582@qq.com>
2021-04-26 13:05:13 +0800

    VueDemo

---
+++ hello-cli4/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <tabbar>
+      <tabbar-item>
+        <template v-slot:item-img>
+          <img slot="item-img" src=".\assets\img\tabbar\home.png" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div slot="item-text">首页</div>
+        </template>
+      </tabbar-item>
+      <tabbar-item>
+        <template v-slot:item-img>
+          <img slot="item-img" src=".\assets\img\tabbar\chiquan.png" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div slot="item-text">吃货圈</div>
+        </template>
+      </tabbar-item>
+      <tabbar-item>
+        <template v-slot:item-img>
+          <img slot="item-img" src=".\assets\img\tabbar\chihuo.png" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div slot="item-text">吃货本货</div>
+        </template>
+      </tabbar-item>
+      <tabbar-item>
+        <template v-slot:item-img>
+          <img slot="item-img" src=".\assets\img\tabbar\chihuo.png" alt="" />
+        </template>
+        <template v-slot:item-text>
+          <div slot="item-text">吃货本货</div>
+        </template>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from "./components/tabbar/Tabbar";
+import TabbarItem from "./components/tabbar/TabbarItem";
+
+export default {
+  name: "App",
+  components: {
+    Tabbar,
+    TabbarItem,
+  },
+};
+</script>
+
+<style>
+/* @import url("./assets/css/base.css"); */
+/* 也可以这种方法引用，但是不推荐 */
+</style>
---
+++ hello-cli4/src/components/tabbar/TabbarItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tab-bar-item">
+    <slot name="item-img"></slot>
+    <slot name="item-text"></slot>
+    <!-- <img src="..\..\assets\img\tabbar\home.png" alt="" />
+    <div>首页</div> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "TabbarItem",
+};
+</script>
+<style scoped>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  /* 去除图片和文字之间的小空间 */
+  margin-bottom: 3px;
+}
+</style>
